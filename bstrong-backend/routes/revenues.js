767426d3e2575557f4f30adb8f4d63472904d105
Devraj Singh <dsrawat@blockchains.com>
2021-04-04 18:57:29 +0200

    revenue record API

---
+++ bstrong-backend/routes/revenues.js
@@ -0,0 +1,193 @@
+const express = require('express')
+const router = express.Router()
+const MemberRecord = require('../models/memberRecord')
+const ExpenseRecord = require('../models/expense')
+
+var revenuePayload = {
+    "netRevenue": '',                          // Sum of all member records till date
+    "netExpense": '',                          // Sum of all Expense records till date
+    "netPending": '',                          // Sum of all Pending records till date
+    "netCompanyBalance": '',                   // Net Revenue - Net Expense
+    "netCashBalance": '',                      // Net Cash Revenue - Net Cash Expense
+    "netBankBalance": '',                      // Net Online Revenue - Net Online Expense
+    "monthlyData": [{                          // Month wise data
+        "month": '',
+        "revenue": '',
+        "expense": '',
+        "pl": '',
+        "registration": '',
+        "renewal": ''
+    }]
+}
+
+router.get('/', async (req, res) => {
+    // console.log('/api/revenues', req)
+    try {
+        // Fetch revenue and expense records (should be fetched year wise)
+        const revenue_records = await MemberRecord.find()
+        const expense_records = await ExpenseRecord.find()
+        let netRevenue = 0
+        let netExpense = 0
+        let netPending = 0
+        let netCashRevenue = 0
+        let netBankRevenue = 0
+        let netCashExpense = 0
+        let netBankExpense = 0
+
+        let monthlyData = [{
+            "SNo": 0,                        // Month wise data
+            "month": 'JAN',
+            "revenue": 0,
+            "expense": 0,
+            "pl": 0,
+            "registration": 0,
+            "renewal": 0
+        }, {
+            "SNo": 1,                        // Month wise data
+            "month": 'FEB',
+            "revenue": 0,
+            "expense": 0,
+            "pl": 0,
+            "registration": 0,
+            "renewal": 0
+        }, {
+            "SNo": 3,                        // Month wise data
+            "month": 'MAR',
+            "revenue": 0,
+            "expense": 0,
+            "pl": 0,
+            "registration": 0,
+            "renewal": 0
+        }, {
+            "SNo": 4,                        // Month wise data
+            "month": 'APR',
+            "revenue": 0,
+            "expense": 0,
+            "pl": 0,
+            "registration": 0,
+            "renewal": 0
+        }, {
+            "SNo": 5,                        // Month wise data
+            "month": 'MAY',
+            "revenue": 0,
+            "expense": 0,
+            "pl": 0,
+            "registration": 0,
+            "renewal": 0
+        }, {
+            "SNo": 6,                        // Month wise data
+            "month": 'JUN',
+            "revenue": 0,
+            "expense": 0,
+            "pl": 0,
+            "registration": 0,
+            "renewal": 0
+        }, {
+            "SNo": 7,                        // Month wise data
+            "month": 'JUL',
+            "revenue": 0,
+            "expense": 0,
+            "pl": 0,
+            "registration": 0,
+            "renewal": 0
+        }, {
+            "SNo": 8,                        // Month wise data
+            "month": 'AUG',
+            "revenue": 0,
+            "expense": 0,
+            "pl": 0,
+            "registration": 0,
+            "renewal": 0
+        }, {
+            "SNo": 9,                        // Month wise data
+            "month": 'SEP',
+            "revenue": 0,
+            "expense": 0,
+            "pl": 0,
+            "registration": 0,
+            "renewal": 0
+        }, {
+            "SNo": 10,                        // Month wise data
+            "month": 'OCT',
+            "revenue": 0,
+            "expense": 0,
+            "pl": 0,
+            "registration": 0,
+            "renewal": 0
+        }, {
+            "SNo": 11,                        // Month wise data
+            "month": 'NOV',
+            "revenue": 0,
+            "expense": 0,
+            "pl": 0,
+            "registration": 0,
+            "renewal": 0
+        }, {
+            "SNo": 12,                        // Month wise data
+            "month": 'DEC',
+            "revenue": 0,
+            "expense": 0,
+            "pl": 0,
+            "registration": 0,
+            "renewal": 0
+        }]
+
+        console.log("Revenue Records --> ", revenue_records)
+        console.log("Expense Records --> ", expense_records)
+
+        // Process revenue records
+        revenue_records.forEach(record => {
+            console.log("Loop Revenue Records --> ", record)
+
+            netRevenue += record.amount;
+            netPending += record.balance;
+
+            if (record.mode == 'Cash') {
+                netCashRevenue += record.amount
+            } else {
+                netBankRevenue += record.amount
+            }
+            // Check the month of Records
+            var date = new Date();
+            var month = date.getMonth(record.txdate);
+            console.log('record.txdate', record.txdate)
+            console.log('Month', month)
+            // monthlyData[month].month = month;
+            monthlyData[month].revenue += record.amount;
+
+            // Count number of registration for fetched month
+            record.txtype == "REGISTRATION" ?
+                monthlyData[month].registration += 1 :
+                monthlyData[month].renewal += 1
+        });
+
+        expense_records.forEach(record => {
+            netExpense += record.amount;
+
+            if (record.mode == 'Cash') {
+                netCashExpense += record.amount
+            } else {
+                netBankExpense += record.amount
+            }
+            // Check the month of Records
+            var date = new Date();
+            var month = date.getMonth(record.txdate);
+            monthlyData[month].expense += record.amount;
+            monthlyData[month].pl = monthlyData[month].revenue - record.amount;
+        });
+
+        revenuePayload.netRevenue = netRevenue;
+        revenuePayload.netExpense = netExpense;
+        revenuePayload.netPending = netPending;
+        revenuePayload.netBankBalance = netBankRevenue - netBankExpense;
+        revenuePayload.netCashBalance = netCashRevenue - netCashExpense;
+        revenuePayload.netCompanyBalance = netRevenue - netExpense;
+        revenuePayload.monthlyData = monthlyData
+        res.json(revenuePayload)
+    } catch (err) {
+        res.send('Error -->' + err)
+    }
+    console.log('Revenue get request')
+})
+
+module.exports = router
